9d36e78972973ad3ae32c5afbb191fcc9a423669
AlexGit40 <oleksandrkolesnyk45@gmail.com>
2025-01-27 21:30:00 +0200

    Add custom hook useIntersectionObserver, update Movies.tsx, moviesSlice.ts, tmdb.ts

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -14,10 +14,10 @@ async function get<TBody>(relativeUrl: string): Promise<TBody> {
 
   const response = await fetch(`${apiBasePath}${relativeUrl}`, requestOptions);
   const value: TBody = await response.json();
-
   return value;
 }
-interface PageResponse<TResult> { 
+
+interface PageResponse<TResult> {
   page: number;
   results: TResult[];
   total_pages: number;
@@ -27,29 +27,39 @@ interface PageResponse<TResult> {
 interface MovieDetails {
   id: number;
   title: string;
-  popularity: number;
   overview: string;
+  popularity: number;
   backdrop_path?: string | null;
 }
 
+interface PageDetails<T> {
+  results: T[];
+  page: number;
+  totalPages: number;
+}
+
 interface Configuration {
-  images:{base_url: string};
+  images: {
+    base_url: string;
+  };
 }
 
-interface ITmdbClient {
+interface ITmbdClient {
   getConfiguration: () => Promise<Configuration>;
-  getNowPlaying: () => Promise<MovieDetails[]>;
+  getNowPlaying: (page: number) => Promise<PageDetails<MovieDetails>>;
 }
 
-export const client: ITmdbClient = {
+export const client: ITmbdClient = {
   getConfiguration: async () => {
     const response = await get<Configuration>("/configuration");
     return response;
   },
-
-getNowPlaying: async () =>{
-    const response = await get<PageResponse<MovieDetails>>("/movie/now_playing");
-
-    return response.results;
+  getNowPlaying: async (page: number = 1) => {
+    const response = await get<PageResponse<MovieDetails>>(`/movie/now_playing?page=${page}`);
+    return {
+      results: response.results,
+      totalPages: response.total_pages,
+      page: response.page,
+    };
   },
 };
